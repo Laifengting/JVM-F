35e39dba1fc24f8ee866319925f41ca6f444e872
Laifengting <laifengting@foxmail.com>
2021-01-15 11:24:27 +0800

    JVM-上篇

---
+++ chapter01/src/com/lft/java/StackStruTest.java
@@ -0,0 +1,20 @@
+package com.lft.java;
+
+/**
+ * Function: 		ADD FUNCTION.
+ * Reason:   		ADD REASON.
+ * Date:            2020-10-12 21:31
+ * <p>
+ * Class Name:      StackStruTest
+ * Package Name:    com.lft.java
+ * @author Laifengting / E-mail:laifengting@foxmail.com
+ * @version 1.0.0
+ * @since JDK 8
+ */
+public class StackStruTest {
+	public static void main(String[] args) {
+		int i = 2;
+		int j = 3;
+		int k = i + j;
+	}
+}
