fe8731ed60df2839d87942275d2c1eb268e378e0
Laifengting <laifengting@foxmail.com>
2021-02-06 12:44:39 +0800

    JVM-上篇

--- chapter02/src/com/atguigu/java/HelloLoader.java
+++ chapter02/src/com/atguigu/java/HelloLoader.java
@@ -5,9 +5,8 @@ package com.atguigu.java;
  * @create 2020 下午 8:12
  */
 public class HelloLoader {
-
-    public static void main(String[] args) {
-        System.out.println("谢谢ClassLoader加载我....");
-        System.out.println("你的大恩大德，我下辈子再报！");
-    }
+	public static void main(String[] args) {
+		System.out.println("谢谢ClassLoader加载我....");
+		System.out.println("你的大恩大德，我下辈子再报！");
+	}
 }
--- chapter02/src/com/atguigu/java1/ClassLoaderTest2.java
+++ chapter02/src/com/atguigu/java1/ClassLoaderTest2.java
@@ -5,21 +5,21 @@ package com.atguigu.java1;
  * @create 2020 上午 10:59
  */
 public class ClassLoaderTest2 {
-    public static void main(String[] args) {
-        try {
-            //1.
-            ClassLoader classLoader = Class.forName("java.lang.String").getClassLoader();
-            System.out.println(classLoader);
-            //2.
-            ClassLoader classLoader1 = Thread.currentThread().getContextClassLoader();
-            System.out.println(classLoader1);
-
-            //3.
-            ClassLoader classLoader2 = ClassLoader.getSystemClassLoader().getParent();
-            System.out.println(classLoader2);
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			//1.
+			ClassLoader classLoader = Class.forName("java.lang.String").getClassLoader();
+			System.out.println(classLoader); // null
+			//2.
+			ClassLoader classLoader1 = Thread.currentThread().getContextClassLoader();
+			System.out.println(classLoader1); // sun.misc.Launcher$AppClassLoader@18b4aac2
+			
+			//3.
+			ClassLoader classLoader2 = ClassLoader.getSystemClassLoader().getParent();
+			System.out.println(classLoader2); // sun.misc.Launcher$ExtClassLoader@677327b6
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
